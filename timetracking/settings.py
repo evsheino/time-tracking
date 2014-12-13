a21be9951a4e031d1d170cb1491b63aa2510cd65
Erkki Heino <evsheino@gmail.com>
2014-12-13 16:23:49 +0200

    Use postgresql in dev

--- timetracking/settings.py
+++ timetracking/settings.py
@@ -75,10 +75,9 @@ DATABASES = {
     }
 }
 
-if os.environ.get('DATABASE_URL', False):
-    # Parse database configuration from $DATABASE_URL
-    import dj_database_url
-    DATABASES['default'] =  dj_database_url.config()
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
