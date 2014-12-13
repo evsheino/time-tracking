981254beb8b0318eda695fb235f6c23dc2d1fc32
Erkki Heino <evsheino@gmail.com>
2014-12-13 15:35:03 +0200

    Initial local db settings

--- timetracking/settings.py
+++ timetracking/settings.py
@@ -49,15 +49,6 @@ ROOT_URLCONF = 'timetracking.urls'
 WSGI_APPLICATION = 'timetracking.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
@@ -74,9 +65,20 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+if os.environ.get('DATABASE_URL', False):
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+    DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
